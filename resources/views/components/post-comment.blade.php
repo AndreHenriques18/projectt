1383bd7dc6bb236feeed086aa4e54aad746655e6
AndreHenriques18 <andrejoao4@msn.com>
2021-09-29 09:19:37 +0100

    Initial commit

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,15 @@
+@props(['comment'])
+<x-panel class="bg-gray-50">
+<article class="flex space-x-4">
+                                <div class="flex-shrink-0">
+                                    <img src="https://i.pravatar.cc/60?u={{$comment->id}}" alt="" width="60" height="60" class="rounded-xl">
+                                </div>
+
+                                <div>
+                                    <header class="mb-4">
+                                        <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                                        <p class="text xs">Posted <time>{{ $comment->created_at }}</time></p>
+                                        <p>{{ $comment->body }}</p>
+                                </header>
+                                </div>
+                            </article></x-panel>
